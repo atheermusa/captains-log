f31bda27d501a18d79c47cea82208f0ff6b24ce4
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-28 11:55:08 +0100

    Changed name of form to AddPlayerForm

---
+++ captains_log/frontend/src/components/pages/AddPlayerForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AddPlayerForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Player Form</h1>
+            </div>
+        )
+    }
+}
+
+export default AddPlayerForm
