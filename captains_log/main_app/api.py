b1ae0da186531237247ad3d8abf89800398b8c45
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-09-28 18:26:22 +0100

    Put in all the models for the database setup

--- captains_log/main_app/api.py
+++ captains_log/main_app/api.py
@@ -1,6 +1,6 @@
-from .models import Captains, MatchReport
+from .models import Captains, MatchReport, SquadInfo, PlayerInfo, TeamInfo, TeamPlayers
 from rest_framework import viewsets, permissions
-from .serializers import CaptainsSerializer, MatchReportSerializer
+from .serializers import CaptainsSerializer, MatchReportSerializer, SquadInfoSerializer, PlayerInfoSerializer, TeamInfoSerializer, TeamPlayersSerializer
 
 class CaptainsViewSet(viewsets.ModelViewSet):
     queryset = Captains.objects.all()
@@ -15,3 +15,31 @@ class MatchReportViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = MatchReportSerializer
+
+class SquadInfoViewSet(viewsets.ModelViewSet):
+    queryset = SquadInfo.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = SquadInfoSerializer
+
+class PlayerInfoViewSet(viewsets.ModelViewSet):
+    queryset = PlayerInfo.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = PlayerInfoSerializer
+
+class TeamInfoViewSet(viewsets.ModelViewSet):
+    queryset = TeamInfo.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = TeamInfoSerializer
+
+class TeamPlayersViewSet(viewsets.ModelViewSet):
+    queryset = TeamPlayers.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = TeamPlayersSerializer
--- captains_log/main_app/serializers.py
+++ captains_log/main_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Captains, MatchReport
+from .models import Captains, MatchReport, SquadInfo, PlayerInfo, TeamInfo, TeamPlayers
 
 class CaptainsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,24 @@ class CaptainsSerializer(serializers.ModelSerializer):
 class MatchReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = MatchReport
+        fields = '__all__'
+
+class SquadInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SquadInfo
+        fields = '__all__'
+
+class PlayerInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerInfo
+        fields = '__all__'
+
+class TeamInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeamInfo
+        fields = '__all__'
+
+class TeamPlayersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeamPlayers
         fields = '__all__'
