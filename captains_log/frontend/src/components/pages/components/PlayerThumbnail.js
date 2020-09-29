6628b0f7759bbf84d25b8e93eec7cda7c0a43328
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-09-29 17:10:17 +0100

    add components and containers for the team sheet functionality

---
+++ captains_log/frontend/src/components/pages/components/PlayerThumbnail.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { PlayerCard } from '.'
+import '../Styles/playerThumbnail.css'
+
+export default class PlayerThumbnail extends Component {
+
+    state = {
+        id: `player${this.props.id}`
+    }
+
+    render() {
+        return (
+            <div className='player-thumbnail-container'>
+                <PlayerCard id = {this.state.id} className="player-thumbnail">
+                    <img className="player-thumbnail-photo" src={this.props.photo} alt="football shirt icon"></img>
+                </PlayerCard>
+                <h2 className= 'player-thumbnail-name' >{this.props.firstName} {this.props.lastName}</h2>
+                <p className= 'player-thumbnail-notes'>Notes: {this.props.notes}</p>
+            </div>
+        )
+    }
+}
