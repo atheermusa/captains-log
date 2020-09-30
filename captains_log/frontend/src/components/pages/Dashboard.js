d82c71bdbfeaaf8e08800d09a905155cc08af16f
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-30 12:42:59 +0100

    Removed player form and list and included buttons to these pages

--- captains_log/frontend/src/components/pages/Dashboard.js
+++ captains_log/frontend/src/components/pages/Dashboard.js
@@ -1,12 +1,34 @@
-import React, { Fragment } from 'react';
-import AddPlayerForm from './AddPlayerForm';
-import PlayerList from './PlayerList';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default function Dashboard() {
-    return (
-        <Fragment>
-            <AddPlayerForm />
-            <PlayerList />
-        </Fragment>
-    )
+
+export class Dashboard extends Component {
+    static propTypes = {
+        auth: PropTypes.object.isRequired,
 }
+
+    render() {
+        const { user } = this.props.auth;
+        return (
+            <Fragment>
+                <h1>Hello {user.username}</h1>
+                <h3>Welcome to your dashboard. From here you can add players to include into your team, or create a team now!</h3>
+                <Link to="/players" className="nav-link"><button className="btn btn-primary">
+                    View/Add Players</button>
+                </Link>
+                <Link to="/teambuilder" className="nav-link"><button className="btn btn-primary">
+                    Create Team</button>
+                </Link>
+            </Fragment>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+  });
+  
+  export default connect(mapStateToProps)(Dashboard);
