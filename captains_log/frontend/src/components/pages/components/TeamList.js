f6227caa24189bc86aece9bc6f84709b7d7a3e6c
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-30 18:26:54 +0100

    Added teams to reducer

--- captains_log/frontend/src/components/pages/components/TeamList.js
+++ captains_log/frontend/src/components/pages/components/TeamList.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { PlayerThumbnail } from '.';
+import { TeamThumbnail } from '.';
 import PropTypes from 'prop-types';
-import { getPlayers, deletePlayers } from '../../../actions/players'
+import { getTeams, deleteTeams } from '../../../actions/teams'
 
 
 export class TeamList extends Component {
 
     static propTypes = {
-        players: PropTypes.array.isRequired,
-        getPlayers: PropTypes.func.isRequired,
-        deletePlayers: PropTypes.func.isRequired
+        teams: PropTypes.array.isRequired,
+        getTeams: PropTypes.func.isRequired,
+        deleteTeams: PropTypes.func.isRequired
     };
 
 
     componentDidMount() {
-        this.props.getPlayers();
+        this.props.getTeams();
     }
 
     render() {
         return (
-            <div id = 'player-list-container'>
-                {this.props.players.map((player, idx) => (
+            <div id = 'team-list-container'>
+                teamslist
+                {this.props.teams.map((teams, idx) => (
               <TeamThumbnail
                 idx={idx}
                 id={idx}
                 key={idx}
-                photo={"https://res.cloudinary.com/dl4pcxjxz/image/upload/v1601304993/cbWFuY2hlc3Rlci11bml0ZWQtc2hpcnQtMjAyMC0yMS1udW1iZXItNy1iZWNraGFtLTM5NDItMDAxLXNxdWFyZQ_aa_qt8ujn.png"}
-                teamname={player.firstname}
-                notes={player.player_notes}
-              />))}
+                photo={"https://res.cloudinary.com/dl4pcxjxz/image/upload/v1601484399/443251-PFJIKT-956-01_wgpvbl.png"}
+                team_name={teams.team_name}
+                formation={teams.formation}
+              />
+              ))}
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    players: state.players.players
+    teams: state.teams.teams
 })
 
-export default connect(mapStateToProps, { getPlayers, deletePlayers })(PlayerList);
+export default connect(mapStateToProps, { getTeams, deleteTeams })(TeamList);
