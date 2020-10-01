48515ebe88286fc011f124f2c2af9be5189fb3b0
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-10-01 20:09:26 +0100

    Added reports css pages

--- captains_log/frontend/src/components/pages/components/ReportsList.js
+++ captains_log/frontend/src/components/pages/components/ReportsList.js
@@ -18,7 +18,7 @@ export class ReportsList extends Component {
     render() {
         return (
             <Fragment>
-                <h2>Reports</h2>
+                <h2 id="report-title">Reports</h2>
                 <table className="table table-striped">
                     <thead>
                         <tr>
