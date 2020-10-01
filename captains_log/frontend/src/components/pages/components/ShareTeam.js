ff5af8107450385dc5fd2f3c8d4b55b7ba83d7fe
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-10-01 15:00:15 +0100

    Added CSS and changed icon of player

--- captains_log/frontend/src/components/pages/components/ShareTeam.js
+++ captains_log/frontend/src/components/pages/components/ShareTeam.js
@@ -1,9 +1,47 @@
-import React, { Fragment } from 'react';
+import
+    
+ React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PlayerThumbnail } from '.';
+import PropTypes from 'prop-types';
+import { getPlayers } from '../../../actions/players'
 
-export default function ShareTeam() {
-    return (
-        <Fragment>
-          <button>Share Team Sheet</button>
-        </Fragment>
-    )
+
+export class ShareTeam extends Component {
+
+    static propTypes = {
+        players: PropTypes.array.isRequired,
+        getPlayers: PropTypes.func.isRequired,
+    };
+
+    state = {
+      playerEmails: []
+    }
+
+    getPlayerEmails = () => {
+      this.props.players.map((player, idx) => (
+        this.state.playerEmails.push([player.email])))
+        console.log(this.state.playerEmails) 
+    }
+
+    componentDidMount() {
+        this.props.getPlayers();
+    }
+
+    render() {
+        return (
+            <div id = 'share-team-container'>
+              <button onClick= {this.getPlayerEmails}>
+              <a href=  "mailto:emai@email.com?subject=Team Lineup">Send Email</a>
+              </button>
+              
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state => ({
+    players: state.players.players
+})
+
+export default connect(mapStateToProps, { getPlayers, })( ShareTeam);
